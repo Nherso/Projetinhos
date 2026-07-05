52249414cd0b8e2bd7a3ad65aeb733fa8db6e6a7
Cauã Bertholasso <134944865+Nherso@users.noreply.github.com>
2026-07-04 21:26:51 -0300

    Update script.js

--- Conversor-Temperatura/script.js
+++ Conversor-Temperatura/script.js
@@ -1,1 +1,35 @@
+const botao = document.getElementById("converter");
 
+botao.addEventListener("click", function () {
+
+    const temperatura = parseFloat(document.getElementById("temperatura").value);
+
+    const tipo = document.getElementById("tipo").value;
+
+    if (isNaN(temperatura)) {
+
+        document.getElementById("resultado").innerHTML =
+        "Digite uma temperatura válida.";
+
+        return;
+    }
+
+    let resultado;
+
+    if (tipo === "CF") {
+
+        resultado = (temperatura * 9 / 5) + 32;
+
+        document.getElementById("resultado").innerHTML =
+        `${temperatura} °C = <strong>${resultado.toFixed(1)} °F</strong>`;
+
+    } else {
+
+        resultado = (temperatura - 32) * 5 / 9;
+
+        document.getElementById("resultado").innerHTML =
+        `${temperatura} °F = <strong>${resultado.toFixed(1)} °C</strong>`;
+
+    }
+
+});
